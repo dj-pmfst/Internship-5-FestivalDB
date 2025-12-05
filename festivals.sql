da1ea7d56e8e66908d46e40b7fbb32967b40bf71
dj-pmfst <djuric@pmfst.hr>
2025-12-05 19:06:34 +0100

    Add enums

--- festivals.sql
+++ festivals.sql
@@ -5,8 +5,8 @@ CREATE TABLE Festivals (
 	max_capacitty INT NOT NULL,
 	start_date TIMESTAMP NOT NULL,
 	end_date TIMESTAMP NOT NULL,
-	status VARCHAR,
-	has_camp BOOLEAN,
+	status Status NOT NULL,
+	has_camp BOOLEAN NOT NULL,
 	stages INT REFERENCES Stages(stages),
 	artists INT REFERENCES Artists(artists)
 )
@@ -15,17 +15,18 @@ CREATE TABLE Stages(
 	stage_id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
 	max_capacity INT NOT NULL,
-	has_cover BOOLEAN,
+	has_cover BOOLEAN NOT NULL,
+	location Location NOT NULL,
 )
 
 CREATE TABLE Artists(
 	artist_id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
 	surname VARCHAR(100),
-	country VARCHAR(100),
-	genre VARCHAR(100) NOT NULL,
-	number_of_members INT,
-	status BOOLEAN,
+	country VARCHAR(100) NOT NULL,
+	genre Genre NOT NULL,
+	number_of_members INT DEFAULT 1,
+	status BOOLEAN NOT NULL,
 	festivals INT REFERENCES Festivals(festivals_id),
 	performances INT REFERENCES Peroformances(performances_id)
 )
@@ -35,9 +36,9 @@ CREATE TABLE Performances(
 	festivals INT REFERENCES Festivals(festivals_id),
 	stage INT REFERENCES Stages(stage_id),
 	artist INT REFERENCES Artists(artist_id),
-	start_time TIMESTAMP,
-	end_time TIMESTAMP,
-	visitor_number INT
+	start_time TIMESTAMP NOT NULL,
+	end_time TIMESTAMP NOT NULL,
+	visitor_number INT NOT NULL
 )
 
 CREATE TABLE Visitors(
@@ -45,8 +46,8 @@ CREATE TABLE Visitors(
 	name VARCHAR(100) NOT NULL,
 	surname VARCHAR(100) NOT NULL,
 	dob TIMESTAMP NOT NULL,
-	city VARCHAR,
-	country VARCHAR,
+	city VARCHAR NOT NULL,
+	country VARCHAR NOT NULL,
 	email VARCHAR NOT NULL,
 	ticket INT REFERENCES Tickets(ticket_id),
 	workshop INT REFERENCES !!!!!!!!!!!!!!!!!!!!
@@ -54,10 +55,10 @@ CREATE TABLE Visitors(
 
 CREATE TABLE Tickets(
 	ticket_id SERIAL PRIMARY KEY,
-	type VARCHAR NOT NULL,
+	type TicketType NOT NULL,
 	price DOUBLE NOT NULL,
-	included VARCHAR,
-	duration VARCHAR NOT NULL
+	included TicketIncluded NOT NULL,
+	duration ValidFor NOT NULL
 )
 
 CREATE TABLE Purchase(
@@ -67,17 +68,17 @@ CREATE TABLE Purchase(
 	purchase_time TIMESTAMP NOT NULL,
 	total_cost DOUBLE NOT NULL,
 	ticket_type VARCHAR REFERENCES Tickets(type),
-	ticket_number INT,
+	ticket_number INT DEFAULT 1,
 )
 
 CREATE TABLE Workshops(
 	workshop_id SERIAL PRIMARY KEY,
 	festival INT REFERENCES Festivals(festival_id),
 	name VARCHAR(100) NOT NULL,
-	difficulty VARCHAR NOT NULL,
-	max_capacity INT,
-	duration TIMESTAMP,
-	prior_experience BOOLEAN,
+	difficulty Difficulty NOT NULL,
+	max_capacity INT NOT NULL,
+	duration TIMESTAMP NOT NULL,
+	prior_experience BOOLEAN NOT NULL,
 )
 
 CREATE TABLE Mentors(
@@ -96,18 +97,21 @@ CREATE TABLE Personnel(
 	dob TIMESTAMP NOT NULL,
 	role VARCHAR NOT NULL,
 	contact INT,
-	safety_training BOOLEAN,
+	safety_training BOOLEAN NOT NULL,
 	festival INT REFERENCES Festivals(festival_id)
 )
 
 CREATE TABLE Memberships(
 	membership_id SERIAL PRIMARY KEY,
 	visitor INT REFERENCES Visitors(visitor_id),
-	status VARCHAR,
-	activation_time TIMESTAMP,
+	status Status NOT NULL,
+	activation_time TIMESTAMP NOT NULL,
 )
 
-
-
-
-
+CREATE TYPE Status AS ENUM ('planned', 'active', 'ended')
+CREATE TYPE Location AS ENUM ('beach', 'forest', 'main')
+CREATE TYPE Genre AS ENUM ('pop', 'rock', 'metal', 'rap', 'country', 'hip-hop', 'jazz', 'electronic')
+CREATE TYPE TicketType AS ENUM ('basic', 'festival', 'camp', 'VIP')
+CREATE TYPE TicketIncluded AS ENUM ('none', 'backstage', 'camp')
+CREATE TYPE ValidFor AS ENUM ('one-day-only', 'entire-festival')
+CREATE TYPE Difficulty AS ENUM ('easy', 'average', 'advanced')
